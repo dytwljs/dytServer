725110247d56b5ce6b8f2a812621d0d52f2083f9
ghw <cnapollo@hotmail.com>
2018-04-19 13:04:51 +0800

    a.1

---
+++ Domain/Entities/RoleMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//using System.ComponentModel.DataAnnotations.Schema;
+namespace dytServer.Domain.Entities
+{
+    public class RoleMenu
+    {
+        public int RoleId { get; set; }
+        public string RoleGUID { get; set; }
+        public Role Role { get; set; }
+
+        public int MenuId { get; set; }
+        public string MenuGUID { get; set; }
+        public Menu Menu { get; set; }
+    }
+}
---
+++ Domain/Entities/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace dytServer.Domain.Entities
+{
+    public class UserRole
+    {
+        public int UserId { get; set; }
+        public string UserGUID { get; set; }
+        public User User { get; set; }
+
+        public int RoleId { get; set; }
+        public string RoleGUID { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
